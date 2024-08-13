84afc0ce1047c865616eb2df69734aff2128d1e0
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-08-13 15:24:57 +0100

    Intial commit

---
+++ lib/priority_queue.ml
@@ -0,0 +1,40 @@
+module type PrioQueue =
+    sig
+        type 'a node
+        type 'a pqueue
+
+        val init : unit -> 'a pqueue
+
+        val enqueue : 'a pqueue -> 'a node -> 'a pqueue
+        val getVal : 'a node -> 'a
+
+        (*
+        val dequeue : 'a pqueue -> 'a option
+        *)
+    end
+
+module MaxHeap : PrioQueue =
+    struct
+        type 'a node = {
+            value : 'a;
+            prio : int;
+            l_child : 'a node option;
+            r_child : 'a node option;
+        }
+
+        type 'a pqueue = {
+            root : 'a node option;
+        }
+
+        let init () : 'a pqueue = {root = None}
+
+        let rec enqueue (q : 'a pqueue) (n : 'a node) : 'a pqueue =
+            match q.root with
+            | None -> { root = Some n }
+            | Some r ->
+                   match r.l_child with
+                   | Some l -> if n.prio> l.prio then enqueue { root=r.l_child } n else enqueue { root=r.r_child } n
+                   | None -> {root=None}
+
+        let getVal (n : 'a node) : 'a = n.value
+    end
