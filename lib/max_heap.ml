a7b8b8ad7f4a5d76abb3bb3b7202fcb365512d7e
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-08-15 21:57:58 +0100

    Add heapify method@

--- lib/max_heap.ml
+++ lib/max_heap.ml
@@ -20,10 +20,10 @@ module MaxHeap =
         let get_max (p :'a node option) (q:  'a node option) : 'a node option =
             match p,q with
             | None, None -> None (* Both none -> None *)
-            | Some node, None -> Some node (* Some node none *)
+            | Some node, None -> Some node (* One node none                   *)
             | None, Some node -> Some node (*               -> the other node *)
             | Some node1, Some node2 -> (* Both some -> greater of the two *)
-                    if node1.value > node2.value then Some node1 else Some node2
+                    if node1.prio > node2.prio then Some node1 else Some node2
 
         let create_node (v : 'a) (p : int) : 'a node =
             {
@@ -38,37 +38,42 @@ module MaxHeap =
         let rec heapify (q : 'a pqueue) : 'a pqueue =
             match q.root with
             | None -> q
-            | Some r ->
-                    match r.l_child, r.r_child with
-                    | None, None -> { root = Some r } (* No children -> return itself *)
+            | Some root ->
+                    match root.l_child, root.r_child with
+                    | None, None -> { root = Some root } (* No children -> return itself *)
                     | Some left, None -> (* Only left child -> swap if larger *)
-                            if left.value > r.value then
-                                let new_root = { left with l_child = Some r } in
+                            if left.prio > root.prio then
+                                let new_left = 
+                                    {root with l_child = left.l_child; r_child = left.r_child} in
+                                let new_left_subtree = heapify { root = Some new_left } in
+                                let new_root = { left with l_child = new_left_subtree.root } in
                                 { root = Some new_root }
-                            else { root = Some r }
+                            else { root = Some root }
                     | None, Some right -> (* Only right child -> swap if larger *)
-                            if right.value > r.value then
-                                let new_root = { right with r_child = Some r} in
+                            if right.prio > root.prio then
+                                let new_right =
+                                    { root with l_child = right.l_child; r_child = right.r_child } in
+                                let new_right_subtree = heapify { root = Some new_right } in
+                                let new_root = { right with r_child = new_right_subtree.root } in
                                 { root = Some new_root }
-                            else { root = Some r }
+                            else { root = Some root }
                     | Some left, Some right ->
-                            match get_max left right with
-                            | left ->
-                                if left.value > r.value then
-                                    let new_root = { left with l_child = Some r } in
-                                    { root = Some new_root }
-                                else
-                                    { root = Some r }
-                            | right ->
-                                if right.value > r.value then
-                                    let new_root = { right with r_child = Some r} in
-                                    { root = Some new_root }
-                                else
-                                    { root = Some r }
-            
+                            match left, right with
+                            | l, _ when l.prio > root.prio ->
+                                let new_left = 
+                                    {root with l_child = l.l_child; r_child = l.r_child} in
+                                let new_left_subtree = heapify { root = Some new_left } in
+                                let new_root = { left with l_child = new_left_subtree.root } in
+                                { root = Some new_root }
+                            | _, r when r.prio > r.prio ->
+                                let new_right = { root with l_child = r.l_child; r_child = r.r_child } in
+                                let new_right_subtree = heapify { root = Some new_right } in
+                                let new_root = { right with r_child = new_right_subtree.root } in
+                                { root = Some new_root }
+                            | _ -> { root = Some root}
 
         let rec push (q : 'a pqueue) (n : 'a node) : 'a pqueue =
             match q.root with
             | None -> { root = Some n }
-            | Some r -> q
+            | Some r ->  push q r
     end
--- test/test_max_heap.ml
+++ test/test_max_heap.ml
@@ -1,68 +1,22 @@
 open Dust.Max_heap
 
-
 let test_init () =
     let mh = MaxHeap.init () in
     match mh.root with
     | None -> ()
     | _ -> Alcotest.fail "Root not None on init"
 
-let test_empty_enque () =
-    let mh = MaxHeap.init () in
-        let node = MaxHeap.create_node "a" 1 in
-            let mh' = MaxHeap.push mh node in
-                match mh'.root with
-                | Some node when node.data = "a" -> ()
-                | _ -> Alcotest.fail "Root not enqueued node"
-
-let test_root_larger () = 
-    let mh = MaxHeap.init () in 
-    let node1 = MaxHeap.create_node "a" 10 in
-    let mh' = MaxHeap.push mh node1 in
-    let node2 = MaxHeap.create_node "b" 1 in
-    let mh'' = MaxHeap.push mh' node2 in
-    match mh''.root with
-    | Some node when node.data = "a" ->
-            (match node.l_child with
-            | Some no when no.data = "b" -> ()
-            | _ -> Alcotest.fail "Lchild not B")
-    | _ -> Alcotest.fail "Root not larger value"
-
-let test_root_smaller () = 
-    let mh = MaxHeap.init () in 
-    let node1 = MaxHeap.create_node "a" 1 in
-    let mh' = MaxHeap.push mh node1 in
-    let node2 = MaxHeap.create_node "b" 10 in
-    let mh'' = MaxHeap.push mh' node2 in
-    match mh''.root with
-    | Some node when node.data = "a" -> (
-            match node.r_child with
-            | Some no when no.data = "b" -> ()
-            | _ -> Alcotest.fail "Lchild not B"
-    )
-    | _ -> Alcotest.fail "Root not larger value"
-
-let test_layer_1_full () =
-    let mh = MaxHeap.init () in 
-    let node1 = MaxHeap.create_node "a" 5 in
-    let mh' = MaxHeap.push mh node1 in
-    let node2 = MaxHeap.create_node "b" 10 in
-    let mh'' = MaxHeap.push mh' node2 in
-    let node3 = MaxHeap.create_node "c" 1 in
-    let mh''' = MaxHeap.push mh'' node3 in 
-    let node4 = MaxHeap.create_node "d" 3 in
-    let mh'''' = MaxHeap.push mh''' node4 in
-    match mh''''.root with
-    | Some node -> (
-        match node.l_child with
-            | Some onode -> (
-                match onode.r_child with
-                | Some enode when enode.data = "d" -> ()
-                | _ -> Alcotest.fail ""
-        )
-            | _ -> Alcotest.fail ""
-    )
-    | _ -> Alcotest.fail "Incorrect"
+let test_heapify () =
+    let ch1 = { MaxHeap.value = "a"; prio = 1; l_child = None; r_child = None } in
+    let r = { MaxHeap.value = "b"; prio = 0; l_child = Some ch1; r_child = None } in
+    let mh = { MaxHeap.root = Some r } in
+    let mh' = MaxHeap.heapify mh in
+    match mh'.root with
+    | Some r when r.value = "a" -> (
+        match r.l_child, r.r_child with
+        | Some left, None when left.value = "b" -> ()
+        | _ -> Alcotest.fail "Expected root.l_child to have value 'b'")
+    | _ -> Alcotest.fail "Expected mh'.root to have value 'a'"
 
 
 
@@ -70,10 +24,7 @@ let test_layer_1_full () =
 let suite =
   [
       "Test init", `Quick, test_init;
-      "Test enqueue empty", `Quick, test_empty_enque;
-      "Test enqueue root greater", `Quick, test_root_larger;
-      "Test enqueue root lesser ", `Quick, test_root_smaller;
-      "Test layer 1 full", `Quick, test_layer_1_full;
+      "Test heapify", `Quick, test_heapify
   ]
 
 let () =
