ed4365014f1175beac26d571758bebee2ac862dd
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-08-15 17:37:49 +0100

    Refactor to heapify

--- lib/max_heap.ml
+++ lib/max_heap.ml
@@ -1,7 +1,13 @@
+module type Ordered =
+    sig
+        type t
+        val leq : t -> t -> bool
+    end
+
 module MaxHeap =
     struct
         type 'a node = {
-            data : 'a;
+            value : 'a;
             prio : int;
             l_child : 'a node option;
             r_child : 'a node option;
@@ -11,19 +17,46 @@ module MaxHeap =
             root : 'a node option;
         }
 
-        let create_node v p = { data = v; prio = p; l_child = None; r_child = None }
+        let get_max (p :'a node option) (q:  'a node option) : 'a node option =
+            match p,q with
+            | None, None -> None (* Both none -> None *)
+            | Some node, None -> Some node (* Some node none *)
+            | None, Some node -> Some node (*               -> the other node *)
+            | Some node1, Some node2 -> (* Both some -> greater of the two *)
+                    if node1.value > node2.value then Some node1 else Some node2
 
-        let init () : 'a pqueue = {root = None}
+        let create_node (v : 'a) (p : int) : 'a node =
+            {
+                value = v;
+                prio = p;
+                l_child = None;
+                r_child = None
+            }
 
+        let init () : 'a pqueue = { root = None }
+
+        let rec heapify (q : 'a pqueue) : 'a pqueue =
+            match q.root with
+            | None -> q
+            | Some r ->
+                    match r.l_child, r.r_child with
+                    | None, None -> { root = Some r } (* No children -> return itself *)
+                    | Some left, None -> (* Only left child -> swap if larger *)
+                            if left.value > r.value then
+                                let new_root = { left with l_child = Some r } in
+                                { root = Some new_root }
+                            else { root = Some r }
+                    | None, Some right -> (* Only right child -> swap if larger *)
+                            if right.value > r.value then
+                                let new_root = { right with r_child = Some r} in
+                                { root = Some new_root }
+                            else { root = Some r }
+                    | Some left, Some right -> q
+
+            
 
         let rec push (q : 'a pqueue) (n : 'a node) : 'a pqueue =
             match q.root with
             | None -> { root = Some n }
-            | Some r ->
-                if r.prio > n.prio then
-                    { root = Some { r with l_child = (push { root = r.l_child } n).root } }
-                else
-                    { root = Some { r with r_child = (push { root = r.r_child } n).root } }
-                        
-        let getVal (n : 'a node) : 'a = n.data
+            | Some r -> q
     end
