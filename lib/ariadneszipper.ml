1e2c48462f3121a17df5b08ee2c46e28ef337d5d
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-09-12 12:43:43 +0100

    Add get, set, apply functions and docs to A's Zipper

--- lib/ariadneszipper.ml
+++ lib/ariadneszipper.ml
@@ -6,6 +6,7 @@
  *)
 
 module AriadnesZipper : sig
+        (* Node representing a point in the labyrinth *)
         type 'a node =
         | DeadEnd of 'a 
         | Passage of 'a * 'a node
@@ -24,11 +25,12 @@ module AriadnesZipper : sig
         (* Thread + current node = zipper *)
         type 'a zipper = 'a thread * 'a node 
 
-        val get : 'a node -> 'a
+        val get : 'a zipper -> 'a
 
-        val set : 'a -> 'a node -> 'a node
+        val set : 'a -> 'a zipper -> 'a zipper
 
         val apply : ('a -> 'a) -> 'a node -> 'a node
+        val apply : ('a -> 'a) -> 'a zipper -> 'a zipper
 
         val keep_straight : 'a zipper -> 'a zipper option
 
@@ -52,6 +54,7 @@ module AriadnesZipper : sig
             | Passage (x, _) -> x
             | Fork (x, _, _) -> x
 
+
         (* Set the value of a node *)
         let set (v : 'a ) (n : 'a node ) : 'a node =
             match n with
@@ -73,9 +76,32 @@ module AriadnesZipper : sig
         | TurnLeft of 'a * 'a node
         | TurnRight of 'a * 'a node
 
+        (* List of branches which describe the path taken to reach an anonymous node *)
         type 'a thread = 'a branch list
 
+        (* Current node and thread to reach the current node *)
         type 'a zipper = 'a thread * 'a node 
+
+        (* Gets the value of the current node represented by the zipper *)
+        let get (z : 'a zipper) : 'a = 
+            match z with
+            | (_, DeadEnd x) -> x
+            | (_, Passage (x, _)) -> x
+            | (_, Fork (x, _, _)) -> x
+
+        (* Sets the value of the current node represented by the zipper *)
+        let set (v : 'a ) (z : 'a zipper) : 'a zipper =
+            match z with
+            | (ts, DeadEnd _) -> (ts, DeadEnd v)
+            | (ts, Passage (_, s)) -> (ts, Passage (v, s))
+            | (ts, Fork (_, l, r)) -> (ts, Fork (v, l, r))
+            
+        (* Applies a function to the value of the current node represented by the zipper *)
+        let apply (f : 'a -> 'a) (z : 'a zipper) : 'a zipper =
+            match z with
+            | (ts, DeadEnd x) -> (ts, DeadEnd (f x))
+            | (ts, Passage (x, s)) -> (ts, Passage (f x, s))
+            | (ts, Fork (x, l, r)) -> (ts, Fork (f x, l, r))
 
         (* Moves the node of the zipper, whilst saving the value of the node
            lost by making move *)
